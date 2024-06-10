c2ca04ff5be7186c5168a5422704053460f16510
Tam <kasperjohan@gmail.com>
2024-06-10 13:15:09 +0200

    Update Update-LoadOrderList.ps1

--- Server/Update-LoadOrderList.ps1
+++ Server/Update-LoadOrderList.ps1
@@ -1,12 +1,16 @@
 Set-Location $PSScriptRoot
 [IO.Directory]::SetCurrentDirectory($PSScriptRoot)
 
+[String]$Dir = (Get-Content information.json | ConvertFrom-JSON).OrderRoot -Replace '/', '\'
+[String]$JSON = (Get-Content information.json | ConvertFrom-JSON).Orders -Replace '/', '\'
+
+
 Try {
     Write-Host -NoNewline 'Writing ''load_orders.json''...'.PadRight($Longest)
 
-    [String[]]$LoadOrders = (Get-ChildItem *.cfg -File).BaseName
+    [String[]]$LoadOrders = (Get-ChildItem "$($Dir)*.cfg" -File).BaseName
 
-    $LoadOrders | ConvertTo-JSON -Compress | Set-Content load_orders.json -NoNewline
+    $LoadOrders | ConvertTo-JSON -Compress | Set-Content "$($Dir)$JSON" -NoNewline
 
     Write-Host -ForegroundColor Green 'OK'
 }
